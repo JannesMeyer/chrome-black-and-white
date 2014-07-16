d274814dba5673590eeae68d79b300aa9b577766
Jannes <jannes.meyer@gmail.com>
2014-07-16 04:05:38 +0200

    Handle errors

--- event-page.js
+++ event-page.js
@@ -1,52 +1,52 @@
 var activationList = {};
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var entry = activationList[tab.id];
-	if (!entry) {
-		entry = { activated: false };
-		chrome.tabs.insertCSS({ file: 'black-and-white.css', allFrames: true });
+var states = {
+	active: {
+		icons: {
+			'19': 'icon-bw-19.png',
+			'38': 'icon-bw-38.png'
+		},
+		title: 'Turn color mode on',
+		code: 'document.documentElement.classList.add("black-and-white-mode")'
+	},
+	inactive: {
+		icons: {
+			'19': 'icon-color-19.png',
+			'38': 'icon-color-38.png'
+		},
+		title: 'Turn black and white mode on',
+		code: 'document.documentElement.classList.remove("black-and-white-mode")'
+	}
+};
+
+function setActive(activate, tabId) {
+	if (!activationList[tabId].possibleToInject) {
+		return;
 	}
 
-	if (!entry.activated) {
-		// activate
-		entry.activated = true;
-		chrome.browserAction.setIcon({
-			path: {
-				'19': 'icon-bw-19.png',
-				'38': 'icon-bw-38.png'
-			},
-			tabId: tab.id
-		});
-		chrome.browserAction.setTitle({
-			title: 'Turn color mode on',
-			tabId: tab.id
-		});
-		chrome.tabs.executeScript({
-			code: 'document.documentElement.classList.add("black-and-white-mode")',
-			allFrames: true
+	activationList[tabId].activated = activate;
+
+	var props = activate ? states.active : states.inactive;
+	chrome.browserAction.setIcon({ path: props.icons, tabId: tabId });
+	chrome.browserAction.setTitle({ title: props.title, tabId: tabId });
+	chrome.tabs.executeScript({ code: props.code, allFrames: true });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var tabId = tab.id;
+	if (!activationList[tabId]) {
+		activationList[tabId] = { possibleToInject: true, activated: false };
+
+		chrome.tabs.insertCSS({ file: 'black-and-white.css', allFrames: true }, function() {
+			if (chrome.runtime.lastError) {
+				activationList[tabId].possibleToInject = false;
+			} else {
+				setActive(true, tabId);
+			}
 		});
 	} else {
-		// deactivate
-		entry.activated = false;
-		chrome.browserAction.setIcon({
-			path: {
-				'19': 'icon-color-19.png',
-				'38': 'icon-color-38.png'
-			},
-			tabId: tab.id
-		});
-		chrome.browserAction.setTitle({
-			title: 'Turn black and white mode on',
-			tabId: tab.id
-		});
-		chrome.tabs.executeScript({
-			code: 'document.documentElement.classList.remove("black-and-white-mode")',
-			allFrames: true
-		});
+		setActive(!activationList[tabId].activated, tabId);
 	}
-
-	// save entry changes
-	activationList[tab.id] = entry;
 });
 
 // TODO: this is not compatible with history.pushState() yet
